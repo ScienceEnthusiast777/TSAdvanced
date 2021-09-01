67f1e28dd15ecd55045101b35edeb08e5ec6154a
alexanderrathbone <alexander.thomas.rathbone@hotmail.com>
2021-09-01 16:38:06 +0200

    end of module

--- src/app.ts
+++ src/app.ts
@@ -142,9 +142,132 @@ const fetchedUserData = {
 
 //nullish coalescing
 const input = null;
-//imagine we dont know if it is null 
-const storedData = input || 'default'
-console.log(storedData)
+//imagine we dont know if it is null
+const storedData = input ?? "default"; //if is null or undefined then get the fallback
+console.log(storedData);
 
+//generics
+const names: Array<string> = [];
+names[0].split(" "); //works because it knows that it should be type string.
+//<T> generic type
+
+// const promise = new Promise<string>((resolve, /*reject*/) => {
+//   setTimeout(() => {
+//     resolve("finished");
+//   }, 2000);
+// });
+
+// promise.then(data => {
+//   data.split(' ')
+// })
+
+// only works because we provided the information that this should resolve to a string
+
+// function merge(objA: object, objB: object) {
+//   return Object.assign(objA, objB);
+// }
+// const merged = merge({ name: "philip" }, { age: 22 });
+// let tester = merged.age
+//wont work
+
+// function merge<T, U>(objA: T, objB: U) {
+//   return Object.assign(objA, objB);
+// }
+// const merged = merge({ name: "philip" }, { age: 22 });
+
+//now this works because TS knows more about whawt needs to be returned
+//allows to be less restrictive, but still give type script more information about what is going on
+
+//type contraints
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+//no we need objects but they can be of any content
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "no val";
+  if (element.length > 0) {
+    descriptionText = "Got " + element.length + "elements.";
+  }
+
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe("Hi there")); // works
+
+//allows you to be specific when you dont partricularly need a specific type but, for éxample here, have a more loose criteria (should have length property)
+
+//key of constraint
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return obj[key];
+}
+
+//here the checker will check that U is a key of T
+
+//generic class.
+//where we want unifrom data
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+  addItem(item: T) {
+    this.data.push(item);
+  }
+  remove(item: T) {
+    this.data.splice(this.data.indexOf(item, 1));
+  }
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+
+textStorage.addItem("hello");
+
+const numberStorage = new DataStorage<number>();
+
+numberStorage.addItem(2);
+
+// const objectStorage = new DataStorage<object>();
+
+// objectStorage.addItem({ name: "peter" });
+
+// console.log(objectStorage.getItems());
+//objects wouldnt work (even without the extension of primitive types above) because we are using objects which are reference types.
+
+//Utility Types
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+//Partial
+function CreateGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+  return courseGoal as CourseGoal; //because otherwise it would have been of type partial, which is not allowed by function
+}
+
+//Readonly
+
+const namesOfDog: Readonly<string[]> = ["bob", "ben", "tilly"];
+// namesOfDog.push("phil");
+//not allowed
+
+//generics are good for locking a certain type for a certain class/ funtion or whatever
 
 
